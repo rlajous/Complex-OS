b39dfebd66f017aa20812dac84335471bce9a5a7
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-03-31 23:42:34 -0300

    Agregando constantes

--- application.h
+++ application.h
@@ -12,6 +12,8 @@
 
 #define NUM_OF_SLAVES 4
 #define STOP_CODE 5
+#define MAX_FILENAME 256
+#define MD5_LENGTH 32
 
 typedef struct {
 	pid_t pid;
@@ -24,5 +26,7 @@ slave* createSlaves(int numberOfSlaves);
 void startApplicationListener(slave * slaves);
 void abortProgram(slave** slaves);
 void stopSlave(slave* slave);
+int readLine(slave* slave, char * buffer);
+void reformatMd5Output(char * output);
 
 #endif
