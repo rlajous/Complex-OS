8bfab0c0881aea0f463194a3b59a8d47c182e84d
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-04-03 06:53:06 -0300

    SharedMemory Lib

---
+++ sharedMemory.c
@@ -0,0 +1,51 @@
+#include "sharedMemory.h"
+
+int * generateSharedMemory(int key, int* memoryId) {
+
+	int * memory;
+
+	//Read & Write Memory
+	*memoryId = shmget(key, sizeof(char) * SHMSIZE, 0606 | IPC_CREAT);
+
+	if (*memoryId == -1) {
+		perror("Shared Memory creation error");
+		exit(1);
+	}
+
+	memory = (int *) shmat(*memoryId, NULL, 0);
+
+	if (memory == NULL) {
+		perror("Shared Memory creation error");
+		exit(1);
+	}
+	return memory;
+}
+
+int generateSemaphore(int key) {
+
+	int semaphoreId;
+
+	semaphoreId = semget (key, 1, 0606 | IPC_CREAT);
+
+	if (semaphoreId == -1) {
+		perror("Semaphore creation error");
+		exit(1);
+	}
+	return semaphoreId;
+}
+
+void initializeSemaphore(int semaphoreId) {
+	union semun sem;
+
+	sem.val = 1;
+	semctl(semaphoreId, 0, SETVAL, &sem);
+}
+
+void changeSemaphore(int semaphoreId, int change) {
+
+	struct sembuf modification;
+	modification.sem_num = 0;
+	modification.sem_op = change;
+	modification.sem_flg = 0;
+	semop(semaphoreId, &modification, 1);
+}
---
+++ sharedMemory.h
@@ -0,0 +1,27 @@
+#ifndef SHAREDMEMORY_H
+#define SHAREDMEMORY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+#define SHMSIZE 100000
+
+int * generateSharedMemory(int key, int* memoryId);
+int generateSemaphore(int key);
+void initializeSemaphore(int semaphoreId);
+void changeSemaphore(int semaphoreId, int change);
+
+//Semun definition from http://man7.org/linux/man-pages/man2/semctl.2.html
+
+union semun {
+  int val;    /* Value for SETVAL */
+  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+  unsigned short  *array;  /* Array for GETALL, SETALL */
+  struct seminfo  *__buf;  /* Buffer for IPC_INFO
+ (Linux-specific) */
+};
+
+#endif
