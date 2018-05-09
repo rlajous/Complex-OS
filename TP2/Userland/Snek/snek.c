6f277bd80390ef8376e01a693002f2c38a20d2d8
Bensas <juanbensa@gmail.com>
2018-05-09 19:46:56 -0300

    Fixed some warnings in snek

--- TP2/Userland/Snek/snek.c
+++ TP2/Userland/Snek/snek.c
@@ -1,9 +1,11 @@
-#include "snek.h"
+#include <snek.h>
 
 int main(int argc, char *argv[]) {
 	player_t player;
 	tail_t tail;
 	snack_t snack; 
+
+	//Initialize using malloc? Or leave it on stack?
 
 	playerAlive = 1;
 	char inputKey;
@@ -49,11 +51,11 @@ void update(player_t* player, tail_t* tail, snack_t* snack){
 
 void draw(player_t* player, tail_t* tail, snack_t* snack){
 	int80(2,0,0,0); //Clear Screen syscall
-	drawCharAt(player.position.x, player.position.y, player.sprite);
+	putcharatpos(player.sprite, player.position.x, player.position.y);
 	for (int i = tail.oldestElemIndex; i < tail.length; i++){
-		drawCharAt(tail.tailPositions[i].x, tail.tailPositions[i].y, tail.sprite);
+		putcharatpos(tail.sprite, tail.tailPositions[i].x, tail.tailPositions[i].y);
 	}
-	drawCharAt(snack.position.x, snack.position.y, snack.sprite);
+	putcharatpos(snack.sprite, snack.position.x, snack.position.y);
 }
 
 void updatePlayer(player_t* player){
@@ -62,29 +64,29 @@ void updatePlayer(player_t* player){
 }
 
 void updateTail(player_t* player, tail_t* tail){
-	*tail.tailPositions[*tail.youngestElemIndex].x = *player.position.x;
-	*tail.tailPositions[*tail.youngestElemIndex].y = *player.position.y;
-	*tail.youngestElemIndex++;
-	*tail.oldestElemIndex++;
-	if (*tail.youngestElemIndex >= TAIL_LENGTH_LIMIT)
-		*tail.youngestElemIndex = 0;
-	if (*tail.oldestElemIndex >= TAIL_LENGTH_LIMIT)
-		*tail.oldestElemIndex = 0;
+	(*tail).tailPositions[(*tail).youngestElemIndex].x = *player.position.x;
+	(*tail).tailPositions[(*tail).youngestElemIndex].y = *player.position.y;
+	(*tail).youngestElemIndex++;
+	(*tail).oldestElemIndex++;
+	if ((*tail).youngestElemIndex >= TAIL_LENGTH_LIMIT)
+		(*tail).youngestElemIndex = 0;
+	if ((*tail).oldestElemIndex >= TAIL_LENGTH_LIMIT)
+		(*tail).oldestElemIndex = 0;
 }
 
 void checkCollisionWithSnack(player_t* player, snack_t* snack){
 	if (*player.position.x = *snack.position.x && *player.position.y = *snack.position.y){
-		*tail.length++;
-		*tail.oldestElemIndex--;
+		(*tail).length++;
+		(*tail).oldestElemIndex--;
 		*snack.position.x = getSeconds() % GAME_WIDTH;
 		*snack.position.y = getSeconds() % GAME_HEIGHT;
 	}
 }
 
 void checkCollisionWithTail(player_t* player, tail_t* tail){
-	for (int i = *tail.oldestElemIndex; i < *tail.length; i++){
-		if (*tail.tailPositions[i].x == *player.position.x && 
-			*tail.tailPositions[i].y == *player.position.y)
+	for (int i = (*tail).oldestElemIndex; i < (*tail).length; i++){
+		if ((*tail).tailPositions[i].x == *player.position.x && 
+			(*tail).tailPositions[i].y == *player.position.y)
 			playerAlive = 0;
 	}
 }
