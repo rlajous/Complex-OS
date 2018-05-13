62dfd8354608bf30d5d41cdb47795f288b487127
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-05-12 21:34:39 -0300

    MessageTest Improved

--- TP2/Userland/MessageTest/messageTest.c
+++ TP2/Userland/MessageTest/messageTest.c
@@ -5,38 +5,50 @@
 int otherProcess(int argc, char * argv[]);
 
 int main(int argc, char *argv[]) {
+  char buffer[50];
+  int length;
   char name[20];
-  char num[11];
-  char * arguments[] = {name, num};
+  char sender[11];
+  char len[2];
+  char * arguments[] = {name, sender, len};
   int recipientPid;
 
   strncpy(name, "messageTestOther", 17);
-  itoa(getpid(), num, 10);
-  recipientPid = runProcess(&otherProcess, 2, arguments);
+  itoa(getpid(), sender, 10);
+
+  printf("Type message: ");
+
+  scanf("%s", buffer);
+
+  length = strlen(buffer);
+
+  itoa(length, len, 10);
+
+  recipientPid = runProcess(&otherProcess, 3, arguments);
 
   createChannel(recipientPid);
 
-  send(recipientPid, "KILL ME", 7);
+  send(recipientPid, buffer, length);
 
   return 0;
 }
 
 int otherProcess(int argc, char *argv[]) {
   int senderPid;
-  char buffer[20];
-  buffer[0] = 'a';
-  buffer[1] = '\0';
+  int length;
+  char buffer[50];
 
   if(argc > 0) {
     parseInt(argv[1],&senderPid);
+    parseInt(argv[2], &length);
   }
   else return -1;
 
   createChannel(senderPid);
 
-  receive(senderPid, buffer, 8);
+  receive(senderPid, buffer, length);
 
-  printf("Process pid: %d received: %s from process %d\n", getpid(), buffer, senderPid);
+  printf("Process pid: %d\nReceived: %s\nFrom process pid %d\n", getpid(), buffer, senderPid);
 
   return 0;
 }
