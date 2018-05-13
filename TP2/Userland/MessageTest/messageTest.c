0f175873e293b0a009645fc411b746dc38fd995a
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-05-12 21:26:21 -0300

    MessageTest

--- TP2/Userland/MessageTest/messageTest.c
+++ TP2/Userland/MessageTest/messageTest.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
 #include <ipc.h>
+#include <process.h>
+
+int otherProcess(int argc, char * argv[]);
 
 int main(int argc, char *argv[]) {
+  char name[20];
+  char num[11];
+  char * arguments[] = {name, num};
+  int recipientPid;
 
-	/*int senderPid;
-	int consumerNumber;
-	char buffer[20];
+  strncpy(name, "messageTestOther", 17);
+  itoa(getpid(), num, 10);
+  recipientPid = runProcess(&otherProcess, 2, arguments);
+
+  createChannel(recipientPid);
+
+  send(recipientPid, "KILL ME", 7);
+
+  return 0;
+}
+
+int otherProcess(int argc, char *argv[]) {
+  int senderPid;
+  char buffer[20];
+  buffer[0] = 'a';
+  buffer[1] = '\0';
 
   if(argc > 0) {
-  	parseInt(argv[0],&senderPid);
-  	parseInt(argv[1],&consumerNumber);
+    parseInt(argv[1],&senderPid);
   }
   else return -1;
 
   createChannel(senderPid);
 
-  printf("%d,%d\n", senderPid, consumerNumber);
+  receive(senderPid, buffer, 8);
 
-  while(1) {
- 		 	receive(senderPid, buffer, 8);
- 		 	printf("Consumer %d consumed: %s\n", consumerNumber,buffer);
-  }*/
-  
+  printf("Process pid: %d received: %s from process %d\n", getpid(), buffer, senderPid);
+
   return 0;
 }
