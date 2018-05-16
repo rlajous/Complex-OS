80b0df2cb035bf45de88d5fd7a6e72f619220b55
Unknown <rnavarro@itba.edu.ar>
2018-05-16 20:29:22 -0300

    Deleted files

--- TP2/test/schedulerTest.h
+++
@@ -1,7 +0,0 @@
-#include "scheduler.h"
-void testSchedulerInitialization();
-void testAddProcess();
-void testRemoveProcess();
-void whenSchedulerIsInitialized();
-void thenCheckSchedulerNotNull();
-void thenSuccess();
