321641d58ae3d2bfb9350228c2fca50bc3bc6cc8
esteban <ekramer@itba.edu.ar>
2018-06-03 13:38:58 -0300

    Added more pipe functions

--- TP2/Kernel/include/pipes.h
+++ TP2/Kernel/include/pipes.h
@@ -6,15 +6,22 @@
 #define MAX_PIPES 10
 
 typedef struct {
+    int pid;
     char name[PROCESS_NAME_LENGTH];
     int readIndex;
     int writeIndex;
     int bytes;
     int buffer[PIPE_BUFFER_LENGTH];
-    int pid;
     int semWrite;
     int semRead;
     int used;
-} Pipe;
+} pipe_t;
+
+
+int createPipe(char * pipeName);
+void initializePipes();
+int getPipe(char * pipeName);
+int writePipe(int pipe, char * message, int bytes);
+int readPipe(int pipe, char * buffer, int bytes);
 
 #endif
--- TP2/Kernel/pipes.c
+++ TP2/Kernel/pipes.c
@@ -1,7 +1,9 @@
 #include <pipes.h>
 #include <scheduler.h>
+#include <semaphore.h>
+#include <lib.h>
 
-static Pipe pipes[MAX_PIPES];
+static pipe_t pipes[MAX_PIPES];
 
 int createPipe(char * pipeName){
     int pid = getpid();
@@ -13,12 +15,12 @@ int createPipe(char * pipeName){
 
     for(i = 0; i < MAX_PIPES; i++) {
         if(pipe[i].used == FALSE){
+            pipes[i].pid = pid;
             size_t nameLength = strlen(pipeName);
             memcpy(pipes[i].name, pipeName);
             pipes[i].readIndex = 0;
             pipes[i].writeIndex = 0;
             pipes[i].bytes = 0;
-            pipes[i].pid = pid;
             return i;
         }
     }
@@ -28,9 +30,10 @@ int createPipe(char * pipeName){
 
 void initializePipes(){
     int i, j;
+    int pid = getPid();
 
     for(i = 0; i < MAX_PIPES; i++){
-        Pipe * p = &pipes[i];
+        pipe_t * p = &pipes[i];
 
         p->readIndex = 0;
         p->writeIndex = 0;
@@ -40,9 +43,63 @@ void initializePipes(){
         }
         p->pid = 0;
         char semWriteName[12] ="SEM_WRITE_";
-
+//      hay que concatenar el nombre con i
+        p->semWrite = createSemaphore(pid, semWriteName);
+        p->semWrite.value = PIPE_BUFFER_LENGTH;
+        char semReadName[11] ="SEM_READ_";
+//      hay que concatenar el nombre con i
+        p->semRead = createSemaphore(pid, semReadName);
 
     }
 }
 
+int getPipe(char * pipeName){
+    int i;
 
+    for(i = 0; i < MAX_PIPES; i++){
+        if(strcmp(pipes[i].name, pipeName) == 0 && pipes[i].used == TRUE){
+            return i;
+        }
+    }
+
+    return createPipe(pipeName);
+}
+
+int writePipe(int pipe, char * message, int bytes){
+    pipe_t * p = &pipes[pipe];
+    int i = 0;
+
+    while(bytes != 0){
+        if(p->bytes == PIPE_BUFFER_LENGTH){
+//            wait() del semWrite no se que pid pedirle
+        }
+        else{
+            p->buffer[p->writeIndex++] = message[i++];
+            p->bytes++;
+            bytes--;
+        }
+    }
+
+//    signal() al semRead
+    return 0;
+}
+
+int readPipe(int pipe, char * buffer, int bytes){
+    pipe_t * p = &pipes[pipe];
+    int i = 0;
+
+    while(bytes != 0){
+        if(p->bytes == 0){
+//            wait al semRead
+        }
+        else{
+            buffer[i++] = p->buffer[p->readIndex++];
+            p->bytes++;
+            bytes--;
+        }
+    }
+
+//    signal al write
+
+    return 0;
+}
